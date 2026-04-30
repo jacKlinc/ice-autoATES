12ebeac48f445f237fef9187e817b69d51b895fe
jacKlinc <jack@inne.io>
2026-04-30 11:03:46 -0600

    Async is slower due to GDAL library serialising concurrent threads

--- tests/e2e/test_area.py
+++ tests/e2e/test_area.py
@@ -17,24 +17,30 @@ AREA_MAP = [
             min_lon=-123.02740693100154,
             max_lon=-122.85407421008304,
         ),
-    },
-    {
-        "name": "mount-seymour",
-        "bbox": BoundaryBox(
-            min_lat=49.334422724929645,
-            max_lat=49.4575084323684,
-            min_lon=-123.02740693100154,
-            max_lon=-122.85407421008304,
-        ),
-    },
-]
+    }
+] * 10
 
 
 def test_avalanche_dataset():
+    # 9.05s for 10 examples
 
     areas = [AvalancheArea(name=a["name"], bbox=a["bbox"]) for a in AREA_MAP]
     d = AvalancheDataset(
         areas=areas, out_dir=Path(__file__), dst_crs=CRS.from_epsg(4326)
     )
     dataset = d.build()
+    assert True
+
+
+@pytest.skip()
+def test_avalanche_dataset_sync():
+    # 6.89s for 10 examples
+    # 
+    areas = [AvalancheArea(name=a["name"], bbox=a["bbox"]) for a in AREA_MAP]
+    d = AvalancheDataset(
+        areas=areas, out_dir=Path(__file__), dst_crs=CRS.from_epsg(4326)
+    )
+    dst_crs = CRS.from_epsg(4326)
+    for area in d.areas:
+        area._download_box(dst_crs)
     assert True
